b12655a7e80b6dabad605a9ff42e6d6fdc0ccb01
Bet4Arrio <emanuelsa-rv@hotmail.com>
2021-11-02 23:39:30 -0300

    correct menssages

--- app/views/users.py
+++ app/views/users.py
@@ -10,24 +10,24 @@ from ..models.cities import Cities, city_schema, cities_schema
 from .cities import new_city
 
 async def post_cities():
-    req_id = request.json['id']
+    user_req_id = request.json['id']
     time  = datetime.datetime.now()
     cities = request.json['cities']
     num_cities = len(cities)
-    req = request_by_id(req_id)
+    req = request_by_id(user_req_id)
     if req:
         return jsonify({'message': 'ID already exists', 'data': {}})
     
-    req = User_requests(req_id, num_cities, time)
+    req = User_requests(user_req_id, num_cities, time)
     
     try:
         result = save_request(req)
         tasks = []
         for city in cities:
-            task = asyncio.create_task(new_city(req_id, city))
+            task = asyncio.create_task(new_city(user_req_id, city))
             tasks.append(task)
         cit = await asyncio.gather(*tasks)
-        return jsonify({'message': 'successfully registered', 'session': result, 'data':cit}), 201
+        return jsonify({'message': 'successfully registered', 'data': {'session': result, 'cities':cit}}), 201
     except:
         return jsonify({'message': 'unable to create', 'data': {}}), 500
 
@@ -41,9 +41,9 @@ def get_request(id):
     req = get_cities_by_user(id)
     if req:
         result = cities_schema.dump(req)
-        user = user_request_schema.dump(request_by_id(id))
-        pct = (len(result)/user["num_cities"])*100
-        return jsonify({'message': f'{pct}%complete', 'data': result})
+        user_req = user_request_schema.dump(request_by_id(id))
+        pct = (len(result)/user_req["num_cities"])*100
+        return jsonify({'message': 'Process status', 'data':{'complete percent': f'{pct}%', 'cities':result}})
     return jsonify({'message': 'ID do not exists', 'data': {}})
 
 
